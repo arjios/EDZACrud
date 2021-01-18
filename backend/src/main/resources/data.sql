b78b9de6741976b8e9ba673f0599e33fb002616a
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-01-17 22:00:42 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_user (name) VALUES ('Antonio');
+INSERT INTO tb_user (name) VALUES ('José');
+INSERT INTO tb_user (name) VALUES ('João');
+INSERT INTO tb_user (name) VALUES ('Pedro');
+INSERT INTO tb_user (name) VALUES ('Maria');
+INSERT INTO tb_user (name) VALUES ('Angelo');
